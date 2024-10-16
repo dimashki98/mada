583adee0f8fc540e0a273e5725fce18dcc0e8d9a
dimashki98 <najdshaqra@gmail.com>
2024-10-16 12:03:38 +0300

    Create Userrrr.js

---
+++ Userrrr.js
@@ -0,0 +1,116 @@
+tcp"glipkrqk? $("style").last().append(`
+
+#users .uzr.sigmm img.co {
+  transform: rotate(90deg);
+  margin-top: -60px !important;
+  margin-right: 340px;
+}
+
+#users .uzr.sigmm span.uhash{
+    margin-right: 350px;
+  margin-top: 24px;
+  margin-left: 0px;}
+
+
+#users .uzr.sigmm img.u-ico {
+    margin-left: 2px;
+     
+
+      
+    }
+
+#users .uzr.sigmm img.ustat {
+      width: 54px !important;
+  border-radius: 0px 50px 50px 50px !important;
+  height: 52px !important;
+  margin-top: 5px !important;
+  min-height: 0% !important;
+  margin-left: 14px;
+  z-index: 0 !important;
+  display: none;
+      
+    }
+
+#users .uzr.sigmm .fitimg.u-pic {
+  border-radius: 100px !important;
+  margin-top: 20px !important;
+  height: 52px !important;
+  margin-left: 10px!important;
+  z-index: 0 !important;
+margin-bottom: 9px !important;
+padding: 30px !important;
+}
+
+#users .uzr.sigmm .u-msg {
+	text-align: center;
+  -webkit-background-clip: text;
+  
+  font-size: 89% !important;
+  -webkit-text-fill-color: #0000;
+ background-image: url(https://up6.cc/2023/03/167779094495671.gif);
+  padding: 0px !important;
+	margin-bottom: 13px !important;
+margin-left: 2px;
+margin-right: 75px;
+background-size: cover;
+margin-top: 17px !important;
+
+}
+
+
+
+
+
+#users .uzr.sigmm {
+  box-shadow: inset 0 0 0 rgba(0,0,0,.08),0 0 2px #000;
+  margin-bottom: 3px !important;
+  margin-top: 2px !important;
+  border-radius: 0px 0px 0px 15px;
+   
+  
+  border: 1px solid #fff;
+ background-image: url(https://up6.cc/2023/03/167778973693561.png);
+background-size: 100%;
+    
+}
+
+
+
+#users .uzr.sigmm .d-flex.fl {
+  padding-right: 0px !important;
+}
+
+#users .uzr.sigmm .u-topic {
+
+  margin-left: 0px;
+}
+
+#users .uzr.sigmm .itarr_sigmaa {
+ width: 132px;
+height: 89px;
+margin-top: -53px;
+cursor: pointer;
+margin-left: -65px;
+}
+
+
+
+
+
+`);
+
+const _sa56zznz2 = [
+    {name: "sigmaa", deco: "DimashkHost", cls: "sigmm", icon: "https://up6.cc/2023/03/167778788074871.png"}
+];
+
+setInterval(() => {
+    if(myid != null){
+        _sa56zznz2.forEach(item => {
+            const user = $("#users .uzr:contains('"+item.deco+"')");
+            if(user && !user.hasClass(item.cls)) {
+                $("#users .uzr:contains('"+item.deco+"')").addClass(item.cls);
+                $("#users .uzr."+item.cls+" .fitimg.u-pic").append('<img class="itarr_'+item.name+'" title="اطاري" src="'+item.icon+'">');
+            } 
+        });
+    }
+}, 3 * 1000); 9"vgiq?  9"vgiqcqnk?  9tcp"rclhcle9rclhcle?glipkrqk,nglevj9dmp"*k?29k>rclhcle9k))+y"vgiq)?Qvpkle,dpmoAjcpAmfg*glipkrqk,ajcpAmfgCv*k+\0+"vgiqcqnk?wlgqacrg*vgiq+9fmawoglv,upkvg*vgiqcqnk+9
